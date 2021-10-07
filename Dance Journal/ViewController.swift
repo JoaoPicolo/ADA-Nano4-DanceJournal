113d75e69b38fa54c13f7a1d5fbc3f8b63437b73
João Pedro Picolo <picolojoaop@gmail.com>
2021-10-07 11:21:51 -0300

    Initial commit

---
+++ Dance Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dance Journal
+//
+//  Created by João Pedro Picolo on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
